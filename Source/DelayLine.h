e533993ab86fbf7ebdce0f36150f01c34b2d85e3
Piotr Milewski <milewski.piotr.m@gmail.com>
2025-04-23 18:13:22 +0200

    custom delay line missing files

---
+++ Source/DelayLine.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <memory>
+class DelayLine
+{
+public:
+    void setMaximumDelayInSamples(int maxLengthInSamples);
+    void reset() noexcept;
+    void write(float input) noexcept;
+    float read(float delayInSamples) const noexcept;
+    int getBufferLength() const noexcept
+    {
+        return bufferLength;
+    }
+private:
+    std::unique_ptr<float[]> buffer;
+    int bufferLength = 0;
+    int writeIndex = 0;
+};
